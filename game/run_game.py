48676708dd31370469a3e437c1c74462f3d76dc3
Tamer <tamerchoyy@gmail.com>
2024-12-27 00:20:16 +0200

    added a class which will run the whole game

--- game/run_game.py
+++ game/run_game.py
@@ -1,87 +1,64 @@
 import pygame as pg
 import configparser
 
-from utils import draw_gradient
 from game import constants
 from field.field import Field
+from main_menu.utils import load_image_from_assets
 
 
-# load user settings
-config = configparser.ConfigParser()
-config.read("CONFIG.cfg")
+class Game:
+    def __init__(self):
+        self.running = True
+        self.screen = pg.display.set_mode(constants.DIMENSIONS)
+        pg.display.set_caption(constants.WINDOW_NAME)
+        self.clock = pg.time.Clock()
 
+        # load settings
+        config = configparser.ConfigParser()
+        config.read("CONFIG.cfg")
 
-# Initialize Pygame
-pg.init()
-
-# Clock for capping FPS
-clock = pg.time.Clock()
-
-
-# Screen settings
-WIDTH, HEIGHT = int(config["Display"]["WIDTH"]), int(config["Display"]["HEIGHT"])
-IS_GRADIENT_BACKGROUND = bool(int(config["Display"]["GRADIENT"]))
-REGULAR_BACKGROUND_COLOR = [int(color) for color in config["Display"]["BACKGROUND_COLOR"].split(", ")]
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Tower-Defense")
-if IS_GRADIENT_BACKGROUND:
-    GRADIENT_START = [int(color) for color in config["Display"]["GRADIENT_START"].split(", ")]
-    GRADIENT_END = [int(color) for color in config["Display"]["GRADIENT_END"].split(", ")]
-    draw_gradient.draw_gradient(
-        screen,
-        GRADIENT_START,
-        GRADIENT_END,
-        config["Display"]["GRADIENT_DIRECTION"].lower()
-    )
-
-
-field_config = {
-    "obstacle_positions": (),
-    "obstacle_images": [
-        pg.transform.scale(
-            pg.image.load("../assets/map_images/blocks/wood_on_grass.png").convert_alpha(),
-            (constants.TILE_SIZE, constants.TILE_SIZE)
+        # create map
+        field_config = {
+            "obstacle_positions": (),
+            "obstacle_images": [
+                pg.transform.scale(
+                    load_image_from_assets(constants.WOOD_ON_GRASS_BLOCK),
+                    (constants.TILE_SIZE, constants.TILE_SIZE)
+                )
+            ],
+            "path_tile": pg.transform.scale(
+                load_image_from_assets(constants.STONE_BLOCK),
+                (constants.TILE_SIZE, constants.TILE_SIZE)
+            ),
+            "path_width": 3,
+            "buildable_tile": pg.transform.scale(
+                load_image_from_assets(constants.GRASS_2_BLOCK),
+                (constants.TILE_SIZE, constants.TILE_SIZE)
+            ),
+            "hover_color_on_tiles": (255, 255, 0, 128)
+        }
+        self.field = Field(
+            (
+                (20, 0), (20, 8), (6, 8), (6, 16), (15, 16), (20, 22), (31, 32)
+            ),
+            (constants.FIELD_ROWS, constants.FIELD_COLS, constants.TILE_SIZE),
+            constants.DIMENSIONS,
+            field_config,
         )
-    ],
-    "path_tile": pg.transform.scale(
-        pg.image.load("../assets/map_images/blocks/stone.png").convert_alpha(),
-        (constants.TILE_SIZE, constants.TILE_SIZE)
-    ),
-    "path_width": 3,
-    "buildable_tile": pg.transform.scale(
-        pg.image.load("../assets/map_images/blocks/grass_2.png").convert_alpha(),
-        (constants.TILE_SIZE, constants.TILE_SIZE)
-    ),
-    "hover_color_on_tiles": (255, 255, 0, 128)
-}
-field = Field(
-    (
-    (20, 0), (20, 8), (6, 8), (6, 16), (15, 16), (20, 22), (31, 32)
-    ),
-    (constants.FIELD_ROWS, constants.FIELD_COLS, constants.TILE_SIZE),
-    (WIDTH, HEIGHT),
-    field_config,
-)
-field.create_field()
+        self.field.create_field()
 
+    def run(self):
+        while self.running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
 
-# Main loop
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+            self.screen.fill((255, 255, 255))
 
+            self.field.render_tiles(self.screen)
 
-    # fill background with some color if the user wants so
-    if not IS_GRADIENT_BACKGROUND:
-        screen.fill(REGULAR_BACKGROUND_COLOR)
+            # Update display
+            pg.display.flip()
+            self.clock.tick(constants.FPS)
 
-    field.render_tiles(screen)
-
-
-    # Update display
-    pg.display.flip()
-    clock.tick(constants.FPS)
-
-pg.quit()
+        pg.quit()
