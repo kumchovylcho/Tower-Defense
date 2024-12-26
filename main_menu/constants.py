6b963b6a1d17683fb7c389485dba15ef6bc6b2ac
Tamer <tamerchoyy@gmail.com>
2024-12-26 16:11:06 +0200

    constants for this package

---
+++ main_menu/constants.py
@@ -0,0 +1,7 @@
+# OPTIONS RELATIVE IMAGE PATHS THAT WILL BE LOADED FROM MAIN_MENU FOLDER
+NORMAL_CLOSE_BUTTON_PATH = "menu_images/options_menu/buttons/close_normal.png"
+HOVER_CLOSE_BUTTON_PATH = "menu_images/options_menu/buttons/close_hover.png"
+
+
+# MAIN MENU RELATIVE IMAGE PATHS THAT WILL BE LOADED FROM MAIN_MENU FOLDER
+MENU_BACKGROUND_FRAME_IMAGE_PATH = "menu_images/initial_menu/background_menu/menu_background.png"
