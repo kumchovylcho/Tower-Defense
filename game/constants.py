cfccbb898085197debb0184acd185d9dd1e363dc
Tamer <tamerchoyy@gmail.com>
2024-12-27 00:20:02 +0200

    moved constants here and added window dimensions

--- game/constants.py
+++ game/constants.py
@@ -1,6 +1,18 @@
+# WINDOW DIMENSIONS
+DIMENSIONS = (1600, 900)
+WINDOW_NAME = "Tower-Defense"
+
+
 FPS = 60
+
 
 # TILE SETTINGS
 TILE_SIZE = 25
 FIELD_ROWS = 32
 FIELD_COLS = 55
+
+
+# GAME ASSET RELATIVE PATHS
+WOOD_ON_GRASS_BLOCK = "map_images/blocks/wood_on_grass.png"
+STONE_BLOCK = "map_images/blocks/stone.png"
+GRASS_2_BLOCK = "map_images/blocks/grass_2.png"
