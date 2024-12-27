4cb0fa82941d9ecaff4758df21e8592b2a0bead4
Tamer <tamerchoyy@gmail.com>
2024-12-27 21:12:16 +0200

    added scale_to option

--- main_menu/utils.py
+++ main_menu/utils.py
@@ -2,10 +2,12 @@ import pygame as pg
 import os
 
 
-def load_image_from_assets(path: str, load_from="../assets") -> pg.Surface:
+def load_image_from_assets(path: str, load_from="../assets", scale_to=None) -> pg.Surface:
     """
     The provided path must start from /assets folder.
     Example: map_images/blocks/grass.png or just image you are inside /assets folder already.
+
+    scale_to: if provided it must be a tuple (to_width, to_height)
     """
     # to prevent path errors such as going too back in the path.
     path = path.lstrip("/")
@@ -16,4 +18,7 @@ def load_image_from_assets(path: str, load_from="../assets") -> pg.Surface:
     assets_dir = os.path.abspath(os.path.join(current_dir, load_from))
     # the final target path
     target_image = os.path.join(assets_dir, path)
-    return pg.image.load(target_image).convert_alpha()
+    loaded_img = pg.image.load(target_image)
+    if scale_to:
+        loaded_img = pg.transform.scale(loaded_img, scale_to)
+    return loaded_img.convert_alpha()
