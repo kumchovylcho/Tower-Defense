5d0d3af4a2405ed6e45810dd864b9a9f8c3ef7a0
Tamer <tamerchoyy@gmail.com>
2024-12-28 01:24:04 +0200

    added util function that safely gets the volume from the cfg file

--- main_menu/utils.py
+++ main_menu/utils.py
@@ -1,5 +1,9 @@
 import pygame as pg
+
 import os
+import configparser
+
+from main_menu import constants
 
 
 def load_image_from_assets(path: str, load_from="../assets", scale_to=None) -> pg.Surface:
@@ -22,3 +26,27 @@ def load_image_from_assets(path: str, load_from="../assets", scale_to=None) -> p
     if scale_to:
         loaded_img = pg.transform.scale(loaded_img, scale_to)
     return loaded_img.convert_alpha()
+
+
+def get_config_file_path() -> str:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # path to the config.cfg file, which is where main.py is located
+    # going up one directory to access the config.ini file
+    return os.path.join(os.path.dirname(base_dir), constants.CONFIG_FILE_NAME)
+
+
+def get_volume_from_config(config: configparser.ConfigParser) -> float:
+    try:
+        volume_value = config.getfloat(constants.CONFIG_SOUND_SECTION,
+                                       constants.CONFIG_SOUND_VOLUME_KEY,
+                                       fallback=constants.CONFIG_DEFAULT_SOUND_VOLUME
+                                       )
+        if volume_value < 0 or volume_value > 1:
+            volume_value = constants.CONFIG_DEFAULT_SOUND_VOLUME
+
+        return volume_value
+    except ValueError as e:
+        # the value that the `CONFIG_SOUND_VOLUME_KEY` holds cant be cast to float, so we return the default value.
+        print(e)
+        return constants.CONFIG_DEFAULT_SOUND_VOLUME
